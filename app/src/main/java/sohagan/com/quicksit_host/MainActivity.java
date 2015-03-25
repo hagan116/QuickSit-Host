6f131cc77838f3f57c8b497e199e12513ef5164f
hagan116 <sohagan@syr.edu>
2015-03-24 20:57:35 -0400

    commit

--- app/src/main/java/sohagan/com/quicksit_host/MainActivity.java
+++ app/src/main/java/sohagan/com/quicksit_host/MainActivity.java
@@ -4,16 +4,47 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
+
+    Button login, register;
+    EditText username, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
 
+
+    public void initViews() {
+
+        username = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.password);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
+        register = (Button) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
