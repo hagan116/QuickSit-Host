864918a9491109c1aa98a686c7a46fc0a708d202
hagan116 <sohagan@syr.edu>
2015-04-05 14:04:10 -0400

    commit

---
+++ app/src/main/java/sohagan/com/quicksit_host/Restaurant.java
@@ -0,0 +1,88 @@
+package sohagan.com.quicksit_host;
+
+import android.location.Location;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
+
+/**
+ *
+ */
+public class Restaurant {
+
+    protected String name;
+    protected String type;
+    private String password;
+    private double latitude;
+    private double longitude;
+
+    protected Location userLocation;
+
+    public Restaurant(String name, String type,double lat, double lon) {
+        this.name = name;
+        this.type = type;
+        this.latitude = lat;
+        this.longitude = lon;
+
+        this.userLocation = new Location("User");
+        userLocation.setLatitude(lat);
+        userLocation.setLongitude(lon);
+
+    }
+
+    public Restaurant(String name, String type, String pass, Location loc){
+        this.name = name;
+        this.type = type;
+        this.password = pass;
+        this.userLocation = loc;
+    }
+
+    public Restaurant(String name, String pass, Location loc){
+        this.name = name;
+        this.password = pass;
+        this.userLocation = loc;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getDist(Location l) {
+        return round((userLocation.distanceTo(l)/1609.34),2);
+    }
+
+    public double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    public double getLat() {
+        return this.latitude;
+    }
+
+    public double getLong(){
+        return this.longitude;
+    }
+    public Location getLoc() {
+        return userLocation;
+    }
+    public void setLoc(Location loc) {
+        this.userLocation = loc;
+    }
+}
