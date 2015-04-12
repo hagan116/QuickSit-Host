6c64b3cc66e53938e03b3296f9e901584cb35e12
malcolmnavarro <mmnavarr@live.com>
2015-04-12 17:01:39 -0400

    AHHUHHH

---
+++ app/src/main/java/sohagan/com/quicksit_host/WaitFragment.java
@@ -0,0 +1,103 @@
+package sohagan.com.quicksit_host;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link WaitFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link WaitFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WaitFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+
+    private String tabName;
+
+    private OnFragmentInteractionListener mListener;
+
+
+    public static WaitFragment newInstance(String tabName) {
+        WaitFragment fragment = new WaitFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, tabName);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public WaitFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            tabName = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_wait, container, false);
+
+
+        TextView waitNum = (TextView) view.findViewById(R.id.wait_num);
+        //waitNum = restaurant.getWait();
+
+
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
